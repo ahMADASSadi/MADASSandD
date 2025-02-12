b73b90be849e4acb79392eaa6e2afc3a5d677267
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-02-12 14:40:40 +0330

    🔧 Update configs
    Configurations such as settings and bash scripts got updated

--- app/views.py
+++ app/views.py
@@ -14,8 +14,8 @@ class HomeGitHubView(TemplateView):
     template_name = "app/index.html"  # The template for the home page
 
     def get_context_data(self, **kwargs):
-        # profile_key = "profile"
-        # repo_key = "repo"
+        profile_key = "profile"
+        repo_key = "repo"
         context = super().get_context_data(**kwargs)
 
         github_username = "ahMADASSadi"  # Your GitHub username
@@ -25,53 +25,52 @@ class HomeGitHubView(TemplateView):
         url = f'https://api.github.com/users/{github_username}'
         repo_url = url+'/repos'
 
-        # user_info = cache.get(profile_key)
-        # if user_info is None:
-        try:
-            response = requests.get(url, timeout=1)
-            response.raise_for_status()
-            data = response.json()
+        user_info = cache.get(profile_key)
+        if user_info is None:
+            try:
+                response = requests.get(url, timeout=1)
+                response.raise_for_status()
+                data = response.json()
 
-            user_info = {
-                'login': data.get('login'),
-                'name': data.get('name'),
-                'bio': data.get('bio'),
-                'email': data.get('email'),
-                'avatar_url': data.get('avatar_url'),
-                'html_url': data.get('html_url'),
-            }
+                user_info = {
+                    'login': data.get('login'),
+                    'name': data.get('name'),
+                    'bio': data.get('bio'),
+                    'email': data.get('email'),
+                    'avatar_url': data.get('avatar_url'),
+                    'html_url': data.get('html_url'),
+                }
 
-            # cache.set(profile_key, user_info, timeout=60*10)
+                cache.set(profile_key, user_info, timeout=60*10)
 
-        except requests.exceptions.RequestException as e:
-
-            print(f"Error fetching GitHub profile data: {e}")
+            except requests.exceptions.RequestException as e:
+                print(f"Error fetching GitHub profile data: {e}")
             user_info = {
                 'error': 'Could not retrieve GitHub profile data'
             }
 
         context['user_info'] = user_info
 
-        # repos_info = cache.get(repo_key)
-        # if repos_info is None:
-        try:
-            response = requests.get(repo_url, auth=("username", github_token), timeout=1)
-            response.raise_for_status()
-            data = response.json()
-            repos_info = [
-                {
-                    'name': repo.get('name'),
-                    'description': repo.get('description'),
-                    'url': repo.get('url')
-                }
-                for repo in data if repo.get('description')
-            ]
+        repos_info = cache.get(repo_key)
+        if repos_info is None:
+            try:
+                response = requests.get(repo_url, auth=("username", github_token), timeout=1)
+                response.raise_for_status()
+                data = response.json()
+                repos_info = [
+                    {
+                        'name': repo.get('name'),
+                        'description': repo.get('description'),
+                        'url': f"http://github.com/{github_username}/{repo.get('name')}"
+                    }
+                    for repo in data if repo.get('description')
+                ]
 
-            # cache.set(repo_key, repos_info, timeout=60*10)
+                cache.set(repo_key, repos_info, timeout=60*10)
 
-        except requests.exceptions.RequestException as e:
+            except requests.exceptions.RequestException as e:
 
-            context['repos'] = None
+                context['repos'] = None
 
         context['repos'] = repos_info
 
--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # Install dependencies
 pip install -r requirements.txt
+pip install pymemcache
 
 # Run Django migrations
 python manage.py migrate
